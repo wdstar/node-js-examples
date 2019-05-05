1cce56523b920f83212b3e9f4ffab73aeb51ad57
wdstar <wdstar@users.noreply.github.com>
2019-05-05 09:37:54 +0000

    add class examples.

--- test/lib/person.test.js
+++ test/lib/person.test.js
@@ -1,15 +1,15 @@
 'use strict';
 
 const assert = require('chai').assert;
+const PersonCtor = require('../../lib/person').PersonCtor;
 const PersonProt = require('../../lib/person').PersonProt;
 const PersonCls = require('../../lib/person').PersonCls;
 
-describe('Person classes', () => {
-  it('creates Person instances.', () => {
-    // 1. Old style class: better as OOP
-    const alice = new PersonProt('Alice');
+describe('PersonCtor classe, old style class, but better as OOP', () => {
+  it('creates a PersonCtor instance.', () => {
+    const alice = new PersonCtor('Alice');
     console.log(alice);
-    // => PersonProt { getName: [Function], setName: [Function] }
+    // => PersonCtor { getName: [Function], setName: [Function] }
     // Encapsulated.
     assert.isUndefined(Object.getOwnPropertyDescriptor(alice, '_name'));
     assert.isUndefined(alice._name);
@@ -21,20 +21,41 @@ describe('Person classes', () => {
     // data property value is a function.
     assert.isFunction(Object.getOwnPropertyDescriptor(alice, 'getName').value);
     assert.isFunction(Object.getOwnPropertyDescriptor(alice, 'setName').value);
+  });
+});
 
-    // 2. ES2015 class
-    const bob = new PersonCls('Bob');
+describe('PersonProt classe, old stlyle prototype base class', () => {
+  it('creates a PersonProt instance.', () => {
+    const bob = new PersonProt('Bob');
     console.log(bob);
-    // => PersonCls { _name: 'Bob' }
-    // `name` property is NOT encapsulated!!
-    assert.equal(Object.getOwnPropertyDescriptor(bob, '_name').value, 'Bob');
+    // => PersonProt { _name: 'Bob' }
+    // `_name` property is NOT encapsulated!!
+    assert.equal(
+        Object.getOwnPropertyDescriptor(bob, '_name').value, 'Bob');
     assert.equal(bob._name, 'Bob');
-    // We should delete get/set from the accessor name.
-    assert.equal(bob.getName, 'Bob');
-    // TypeError: bob.getName is not a function
-    // assert.equal(bob.getName(), 'Bob');
-    // each accessor is NOT an accessor property
+    assert.equal(bob.getName(), 'Bob');
+    // AssertionError: expected [Function] to equal 'Bob'
+    // assert.equal(bob.getName, 'Bob');
     assert.isUndefined(Object.getOwnPropertyDescriptor(bob, 'getName'));
     assert.isUndefined(Object.getOwnPropertyDescriptor(bob, 'setName'));
+  });
+});
+
+describe('PersonCls ES2015 classe', () => {
+  it('creates a PersonCls instance.', () => {
+    const carol = new PersonCls('Carol');
+    console.log(carol);
+    // => PersonCls { _name: 'Carol' }
+    // `_name` property is NOT encapsulated!!
+    assert.equal(
+        Object.getOwnPropertyDescriptor(carol, '_name').value, 'Carol');
+    assert.equal(carol._name, 'Carol');
+    // We should delete get/set from the accessor name.
+    assert.equal(carol.getName, 'Carol');
+    // TypeError: carol.getName is not a function
+    // assert.equal(carol.getName(), 'Carol');
+    // each accessor is NOT an accessor property
+    assert.isUndefined(Object.getOwnPropertyDescriptor(carol, 'getName'));
+    assert.isUndefined(Object.getOwnPropertyDescriptor(carol, 'setName'));
   });
 });
